e41450607aa38eb356344e10664cc6a707ddf9b5
Michelle-0793 <mlopez@fwdcostarica.com>
2024-08-20 23:49:46 -0600

    codigo js del formulario e implementación de get y post

---
+++ src/js/formulario.js
@@ -0,0 +1,58 @@
+import { postSolicitud } from "../servicios/postSolicitud";
+import { getSolicitud } from "../servicios/getSolicitud";
+
+
+const nombre = document.getElementById("nombre");
+const codigoComputadora = document.getElementById("codigoComputadora");
+const sede = document.getElementById("sede");
+const fechaSalida = document.getElementById("fechaSalida");
+const fechaRegreso = document.getElementById("fechaRegreso");
+const terminosCondiciones = document.getElementById("terminosCondiciones");
+const btnEnviar = document.getElementById("btnEnviar");
+const btnHistorial = document.getElementById("btnHistorial");
+const mensaje = document.getElementById("mensaje");
+
+
+// Función para manejar el evento click del botón "Enviar"
+async function enviarSolicitud() {
+
+// Validar que todos los campos estén llenos
+    if (!nombre.value || !codigoComputadora.value || !sede.value || !fechaSalida.value || !fechaRegreso.value) {
+        mensaje.textContent = "Por favor, complete todos los campos";
+        return;
+    }
+
+// Validar que se hayan aceptado los términos y condiciones
+    if (!terminosCondiciones.checked) {
+        mensaje.textContent = "Acepte los términos y condiciones";
+        return;
+    }
+
+//Obtener las solicitudes existentes
+const solicitudes = await getSolicitud();
+
+// Nueva solicitud con los datos del formulario
+const nuevaSolicitud = {
+    nombreUsuario: nombre.value,
+    codigoComputadora: codigoComputadora.value,
+    sede: sede.value,
+    fechaSalida: fechaSalida.value,
+    fechaRegreso: fechaRegreso.value,
+    };
+
+// Limpiar los campos
+nombre.value = "";
+codigoComputadora.value = "";
+sede.value = "";
+fechaSalida.value = "";
+fechaRegreso.value = "";
+
+// Enviar la nueva solicitud usando postSolicitudes
+    const response = await postSolicitud(nuevaSolicitud);
+    mensaje.textContent = "Solicitud enviada exitosamente";
+
+}
+
+
+// Asignar la función al evento click del botón
+btnEnviar.addEventListener("click", enviarSolicitud);
---
+++ src/servicios/getSolicitud.js
@@ -0,0 +1,21 @@
+async function getSolicitud() {
+    try {
+        // Realiza una solicitud GET a la URL especificada para obtener las solicitudes
+        const response = await fetch('http://localhost:3001/solicitudes', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Espera la respuesta en formato JSON
+        const data = await response.json();
+        // Retorna los datos obtenidos de la respuesta del servidor
+        return data;
+    } catch (error) {
+        // Captura y muestra cualquier error que ocurra durante la solicitud
+        console.error(error);
+    }
+}
+
+export { getSolicitud };
