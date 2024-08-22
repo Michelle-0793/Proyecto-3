21c54d645b25791b2c43d91352a5481bf2459ba2
yancyalfaro1 <yalfaro@fwdcostarica.com>
2024-08-22 00:50:20 -0600

    Avance login

--- src/js/login.js
+++ src/js/login.js
@@ -1,22 +1,32 @@
 import { getUsers } from "../services/getUsuarios"
 
 const cedula = document.getElementById("cedula")
-const contraseña = document.getElementById("contraseña")
+const contrasena = document.getElementById("contrasena")
 const btnLogin = document.getElementById("btnLogin")
 
 btnLogin.addEventListener("click", function () {
 
  validarUsuario()
 
-  async function validarUsuario() {
-  const lista = await getUsers()
-  for (let index = 0; index < lista.length; index++) {
+    async function validarUsuario() {
+     const lista = await getUsers()
+
+        for (let index = 0; index < lista.length; index++) {
+   
+           if (lista[index].cedula === cedula.value && lista[index].contrasena === contrasena.value) {
+             let usuarioDatos = lista [index].nombre
+             localStorage.setItem("usuarioDatos", (usuarioDatos))
+             console.log("Usuario registrado")
+             window.location.href = "Registro.html"
+
+            }else{
+             console.log("Usuario no registrado")
+            }
     
-    
-  }
+         }
 
     
- }
+    }
     
 })
 
